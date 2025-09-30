7cd3108c478d64dbadaaddf951691a699ee3384f
Hayzer <lucashyzr@gmail.com>
2025-09-30 14:41:25 -0300

    feat: created form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import { lazy, Suspense, useCallback, useState } from "react";
 import { Home } from "./pages/Home"
+import type { StudySession } from "./types/studySession";
+
+const AddStudySession = lazy(() =>
+  import("./pages/add-study-session").then((module) => ({
+    default: module.AddStudySession,
+  }))
+);
 
 function App() {
+  const [studySessions, setStudySessions] = useState<StudySession[]>([]);
 
+  const addStudySession = useCallback((studySession: StudySession) => {
+    setStudySessions((prev) => [...prev, studySession]);
+  }, []);
   return (
     <>
     <Home />
+    <Suspense fallback={<div>Loading...</div>}>
+      <AddStudySession studySessions={studySessions} onAdd={addStudySession} />
+    </Suspense>
     </>
   )
 }
